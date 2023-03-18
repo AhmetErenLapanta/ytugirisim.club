7ffa4e85d88c6b12b5ee971b515a194f8488e8ec
Lapanta09 <ahmet.lapanta01@hotmail.com>
2023-03-18 15:35:35 +0300

    bug fix

--- ytugirisim_client/src/sections/BlogCards.jsx
+++ ytugirisim_client/src/sections/BlogCards.jsx
@@ -3,31 +3,31 @@ import BlogCard from "@/components/BlogCard";
 import Link from "@/components/Link";
 
 const BlogCards = () => {
-    const [idList, setIdList] = useState([]);
-    const [content, setContent] = useState();
+    // const [idList, setIdList] = useState([]);
+    // const [content, setContent] = useState();
 
-    // Fetch blog data on component mount
-    useEffect(() => {
-        const fetchData = async () => {
-            const blogResponse = await fetch(
-                `${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs?populate=*`
-            );
-            const data = await blogResponse.json();
+    // // Fetch blog data on component mount
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const blogResponse = await fetch(
+    //             `${process.env.NEXT_PUBLIC_STRAPI_URL}/blogs?populate=*`
+    //         );
+    //         const data = await blogResponse.json();
 
-            // Get the last three IDs in the list and store them in state
-            const newIdList = data.data.map((blog) => blog.id);
-            setIdList(newIdList.slice(-3));
+    //         // Get the last three IDs in the list and store them in state
+    //         const newIdList = data.data.map((blog) => blog.id);
+    //         setIdList(newIdList.slice(-3));
 
-            // Store blog data in state
-            setContent(data.data);
-        };
+    //         // Store blog data in state
+    //         setContent(data.data);
+    //     };
 
-        fetchData();
-    }, []);
+    //     fetchData();
+    // }, []);
 
     return (
         <div className="section">
-            <h2 className="section-title">Son Bloglarımız</h2>
+            {/* <h2 className="section-title">Son Bloglarımız</h2>
             <section className="blog__container bd-grid">
                 {idList.map((id) => {
                     // Get the blog data for this ID
@@ -35,7 +35,7 @@ const BlogCards = () => {
                     // Render a BlogCard component with this data
                     return <BlogCard key={id} id={id} content={blogContent} />;
                 })}
-            </section>
+            </section> */}
             <Link href="/blogs" type="">
                 <button className="button2">Tüm Bloglar!</button>
             </Link>
